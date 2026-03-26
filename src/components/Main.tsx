4d19621ad46529613d330738253905289f53af66
Igor Jorganovic <igor.jorganovic@bring.com>
2026-03-26 14:02:23 +0100

    refactor: add map as an option

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,14 @@
 import parcelPicker from "../../script/index";
+// @ts-ignore
+import map from '../map.png';
 
 parcelPicker()
 
 const Main = () => {
   return (
-    <></>
+    <div style={{ height: '100%', width: '100%' }}>
+        {/*<img src={map} alt="Map" style={{ objectFit: 'cover', width: '100%', height: '100%', }} />*/}
+    </div>
   )
 }
 
